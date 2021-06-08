3c8ab481bce9594e65e0598c702de45c1b6e0386
github username <github email address>
2021-06-08 11:14:55 -0400

    added tailwind cdn

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 // next.config.js
 module.exports = {
-	future: {
-		webpack5: true,
-	},
-	webpack: function (config, options) {
-		console.log(options.webpack.version); // 5.18.0
-		config.experiments = {};
-		return config;
-	},
+	webpack5: true,
+
 	images: {
 		domains: [
 			'img.huffingtonpost.com',
