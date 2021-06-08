e207aadf7822cd8afd30265aa54f5488a9f0dbee
github username <github email address>
2021-06-08 10:49:38 -0400

    added webpack false to next config file to use webpac4

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 // next.config.js
 module.exports = {
+	future: {
+		webpack5: true,
+	},
+	webpack: function (config, options) {
+		console.log(options.webpack.version); // 5.18.0
+		config.experiments = {};
+		return config;
+	},
 	images: {
 		domains: [
 			'img.huffingtonpost.com',
@@ -9,5 +17,5 @@ module.exports = {
 	},
 	// Webpack 5 is enabled by default
 	// You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
-	webpack5: false,
+	// webpack5: false,
 };
